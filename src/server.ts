926df1c11f8a8f39d8c08685eb8c5d57dfcbfbbe
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2024-07-26 05:19:49 -0300

    feat: add validation and hashing to user creation

--- src/server.ts
+++ src/server.ts
@@ -28,3 +28,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log(`Servidor rodando em http://localhost:${PORT}`)
 );
+
+console.clear();
--- src/services/users/CreateUserService.ts
+++ src/services/users/CreateUserService.ts
@@ -1,6 +1,8 @@
 // Importa o cliente Prisma para interagir com o banco de dados
 import prismaClient from "../../prisma";
 
+import { hash } from "bcryptjs";
+
 // Define a interface para a requisição de criação de usuário, contendo nome, email e senha
 interface UserRequest {
   name: string;
@@ -11,15 +13,41 @@ interface UserRequest {
 class CreateUserService {
   // Método assíncrono para executar a criação do usuário
   async execute({ name, email, password }: UserRequest) {
+    if (!email) {
+      throw new Error(
+        "Oops! Parece que o formato do e-mail não está correto. Certifique-se de digitar um endereço de e-mail válido. "
+      );
+    }
+
+    // Verifica se o email já está em uso
+    const emailAlreadyExist = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (emailAlreadyExist) {
+      throw new Error(
+        "Ops! Parece que esse e-mail já está em uso. Por favor, escolha outro ou faça login com sua conta existente."
+      );
+    }
+
+    // Faz o hash da senha antes de salvar no banco de dados
+    const passwordHash = await hash(password, 8);
+
+    // Cria um novo usuário no banco de dados
     const user = await prismaClient.user.create({
       data: {
         name: name,
         email: email,
-        password: password,
+        password: passwordHash,
+      },
+      select: {
+        name: true,
+        email: true,
       },
     });
 
-    // Retorna o usuário criado
     return user;
   }
 }
